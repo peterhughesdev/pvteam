1e47f555e8fd649b57f93c6df5fe60d5130a4f4a
Peter Hughes <peterhughes.dev@gmail.com>
2015-02-04 23:21:27 +0000

    Add recent post to homepage

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,4 +1,6 @@
 var keystone = require('keystone');
+var Page = keystone.list('Page').model;
+var Post = keystone.list('Post').model;
 
 exports = module.exports = function(req, res) {
 	
@@ -8,7 +10,24 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
-	
+    locals.data = {};
+
+    view.on('init', function(next) {
+
+        Page.findOne({
+                slug: 'index'
+        }).exec(function(err, page) {
+            locals.data.page = page;
+
+        Post.findOne({ state : 'published' })
+            .sort('-publishedDate')
+            .exec(function(err, post) {
+                locals.data.post = post;
+                next(err);
+            });
+        });
+    });
+
 	// Render the view
 	view.render('index');
 	
--- templates/views/helpers/index.js
+++ templates/views/helpers/index.js
@@ -107,7 +107,7 @@ module.exports = function() {
 			if (autolink) {
 				return _.map(tags, function(tag) {
 					return linkTemplate({
-						url: ('/blog/' + tag.key),
+						url: ('/news/' + tag.key),
 						text: _.escape(tag.name)
 					});
 				}).join(separator);
@@ -216,18 +216,18 @@ module.exports = function() {
 	
 	// Direct url link to a specific post
 	_helpers.postUrl = function(postSlug, options) {
-		return ('/blog/post/' + postSlug);
+		return ('/news/post/' + postSlug);
 	};
 	
 	// might be a ghost helper
 	// used for pagination urls on blog
 	_helpers.pageUrl = function(pageNumber, options) {
-		return '/blog?page=' + pageNumber;
+		return '/news?page=' + pageNumber;
 	};
 	
 	// create the category url for a blog-category page
 	_helpers.categoryUrl = function(categorySlug, options) {
-		return ('/blog/' + categorySlug);
+		return ('/news/' + categorySlug);
 	};
 	
 	// ### Pagination Helpers
