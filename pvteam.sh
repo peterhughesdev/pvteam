bf699c7ee064658bd7257ee14711ca7f43461330
Peter Hughes <peterhughes.dev@gmail.com>
2015-03-30 19:38:18 +0100

    Actually make startup script work

--- pvteam.sh
+++ pvteam.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-WEBSITE_DIR=/home/jw9863/website/pvteam
-#WEBSITE_DIR=/Users/Peter/Dev/Projects/pvteam
+#WEBSITE_DIR=/home/jw9863/website/pvteam
+WEBSITE_DIR=/Users/Peter/Dev/Projects/pvteam
 
-LOG_DIR=/home/jw9863/website/logs
-#LOG_DIR=/Users/Peter/Dev/Projects/pvteam-logs
+#LOG_DIR=/home/jw9863/website/logs
+LOG_DIR=/Users/Peter/Dev/Projects/pvteam-logs
 
-export NODE_ENV=dev
+export NODE_ENV=production
 export PORT=3000
 
 #mongod Do this in production mode
 
-forever start -a -l $LOG_DIR/log.log -o $LOG_DIR/out.log -e $LOG_DIR/err.log $WEBSITE_DIR/keystone.js
+forever start -a -l $LOG_DIR/log.log -o $LOG_DIR/out.log -e $LOG_DIR/err.log --workingDir $WEBSITE_DIR $WEBSITE_DIR/keystone.js
